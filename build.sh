f641c50daaecf2bc64e660bb257347b7aa35d3ff
Javier Cabrera <javierca@kth.se>
2019-08-06 18:41:27 +0200

    Manual release deployment

--- build.sh
+++ build.sh
@@ -7,4 +7,5 @@ mkdir dist
 mvn clean package -DskipTests
 cp STRACAlign/target/STRAC-align-0.1.jar dist/STRAC.jar
 cp -R STRACAlign/target/dependency-jars dist/dependency-jars
-ls
+
+zip -r -o dist/STRAC.zip dist/*
--- build_travis.sh
+++ build_travis.sh
@@ -1,6 +1,1 @@
-
 mvn clean package -DskipTests
-cp /home/travis/build/Jacarte/STRAC/STRACAlign/target/STRAC-align-0.1.jar /home/travis/build/Jacarte/STRAC/dist/STRAC.jar
-cp -R /home/travis/build/Jacarte/STRAC/STRACAlign/target/dependency-jars /home/travis/build/Jacarte/STRAC/dist/dependency-jars
-
-ls /home/travis/build/Jacarte/STRAC/dist
