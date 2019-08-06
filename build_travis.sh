29d4ae4786e64a01dbddb25998a3d9e3ae7d4667
Javier Cabrera <javierca@kth.se>
2019-08-06 18:26:25 +0200

    Travis build script

--- build_travis.sh
+++ build_travis.sh
@@ -1,10 +1,5 @@
-rm -rf /home/travis/build/Jacarte/STRAC/STRACAlign/target
-rm -rf /home/travis/build/Jacarte/STRAC/STRACcore/target
-rm -rf /home/travis/build/Jacarte/STRAC/target
-rm -rf /home/travis/build/Jacarte/STRAC/dist/
-mkdir dist
 
 mvn clean package -DskipTests
-cp /home/travis/build/Jacarte/STRAC/STRACAlign/target/STRAC-align-0.1.jar dist/STRAC.jar
-cp -R /home/travis/build/Jacarte/STRAC/STRACAlign/target/dependency-jars dist/dependency-jars
-ls
+cp STRACAlign/target/STRAC-align-0.1.jar dist/STRAC.jar
+cp -R STRACAlign/target/dependency-jars dist/dependency-jars
+ls dist
